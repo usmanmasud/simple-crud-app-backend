ee049adabd7ab177b021ade49f80b8e61c0a2cf4
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-11-12 15:26:16 +0100

    working

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const Product = require("../models/product.model.js");
 const router = express.Router();
-const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller.js');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller.js");
 
-
-router.get('/', getProducts);
+router.get("/", getProducts);
 router.get("/:id", getProduct);
 
 router.post("/", createProduct);
@@ -14,8 +19,5 @@ router.put("/:id", updateProduct);
 
 // delete a product
 router.delete("/:id", deleteProduct);
-
-
-
 
 module.exports = router;
